2f923a7f03d86402c9fedac8adee0fe304d9956a
jacksonm <jacksonm@getamplifylife.com>
2024-12-27 14:18:32 -0800

    not sure

--- config.lua
+++ config.lua
@@ -10,7 +10,6 @@ lvim.format_on_save = true
 vim.opt.relativenumber = true
 
 lvim.builtin.project.manual_mode = true
-
 lvim.builtin.nvimtree.setup = {
   sync_root_with_cwd = false,
   respect_buf_cwd = true,
